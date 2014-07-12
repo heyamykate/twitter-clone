1e5e7bf7e7dfdbb701fed968ae095452daf57bfd
Amy Andrews <amyandrews@Amys-MacBook-Pro.local>
2014-07-12 13:01:22 -0600

    updating

--- js/app.js
+++ js/app.js
@@ -12,24 +12,18 @@ var doubleHeight = function() {
 	});
 }
 
-
 //STEP 2: When the user clicks on the textarea, the textarea should double in size and the character count and Tweet buttons should be revealed. -->
 //HINT: jQuery ".on" eventHandler
 
 $(document).ready(function() {
-	//Step 1: capture click in text area
 	dashTweet.click(function() {
 		doubleHeight();
 		$('#tweet-controls').toggle();
 	})
 
-
-
-
 //STEP 3: As the user types the character count should decrease. Once it hits 10 character or less the count should turn red
 	
 	dashTweet.keyup(function() {
-		//alert("keypress is happening") = WORKS
 		tweetLength.text(140 - dashTweet.val().length);
 		var charactersLeft = tweetLength.text();
 		if(charactersLeft <= 10) {
@@ -53,30 +47,45 @@ $(document).ready(function() {
 //STEP 5: When the user successfully inputs characters and clicks the “Tweet” button, a new tweet should be created and added to the tweet stream in the main column, using the user’s fake profile image in the top left and username/fullname. -->
 //HINT: jQuery ".prepend" method
 
-//Capture click on Tweet (tweetSend) button
 $('#newTweet').hide();
 
 tweetSend.on("click", function() {
-	//Prepend user's tweet into the div #stream
-	var tweet = ('#newTweet').clone(true);
+	var tweet = $('#newTweet').clone(true);
 	tweet.removeAttr('id');
 	tweet.css({display: 'block'});	
-
 	$('#stream').prepend(tweet);
+	$('#newTweetText').text($('.tweet-compose').val());
+})
+
+//STEP 6: The tweet actions below should only show up when you hover over the tweet. Otherwise, they should be hidden. -->
+$('.tweet-actions').hide();
+$('div.tweet').hover(function() {
+		$('.tweet-actions').toggle();
+	});
+
+//STEP 7: The Retweets/timestamp/Reply areas below should also be hidden by default. These should only expand if you click on the tweet. Use a jQuery animation to accomplish the reveal, similar to how it’s done on Twitter.com -->
+
+$('div.stats').hide();
+
+$('div.tweet').on("click", function() {
+	$('div.stats').toggle();
 })
 
 
 
-//Use username and profile pic and handle
+//BLACK DIAMOND: Implement the Bootstrap tooltips for when you hover over a user’s avatar image -->
+//HINT: Bootstrap has good documentation =) -->
 
-//give the post the class of .tweet
 
 
-//STEP 6: The tweet actions below should only show up when you hover over the tweet. Otherwise, they should be hidden. -->
-//HINT: CSS ":hover" pseudo element -->
+//BLACK DIAMOND: Make the timestamp below similar to how they look on Twitter (1h, 18m, 1m) and use the jQuery timeago plugin to make them automatic. -->
+//HINT: Refer to timeago documentation -->
 
-//STEP 7: The Retweets/timestamp/Reply areas below should also be hidden by default. These should only expand if you click on the tweet. Use a jQuery animation to accomplish the reveal, similar to how it’s done on Twitter.com -->
-//HINT: jQuery ".on" eventHandler -->
+
+//BLACK DIAMOND: Implement the icons for when a tweet is favorited/retweeted in the upper right of the tweet. -->
+//HINT: Add to what you created in Step 5 -->
+
+
 
 });
 
